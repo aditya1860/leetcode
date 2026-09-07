fd31853d9f47d3759537c65d95c8dab6f2f28fa7
Aditya joshi <adityajoshi123234@gmail.com>
2026-09-08 01:18:54 +0530

    Time: 124 ms (23.61%), Space: 134.2 MB (26.28%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -1,23 +1,26 @@
+#include <vector>
+#include <unordered_set>
+
 class Solution {
 public:
-    bool containsNearbyDuplicate(vector<int>& nums, int k) {
-
-        unordered_set<int> window;
-
-        for (int i = 0; i < nums.size(); i++) {
-
-            if (window.count(nums[i])) {
+    bool containsNearbyDuplicate(std::vector<int>& nums, int k) {
+        std::unordered_set<int> set;
+        
+        for (int i = 0; i < nums.size(); ++i) {
+            // Remove the element that is now outside the window
+            if (i > k) {
+                set.erase(nums[i - k - 1]);
+            }
+            
+            // Check if the current element exists in the set
+            if (set.count(nums[i])) {
                 return true;
             }
-
-            window.insert(nums[i]);
-
-            // Keep only the last k elements
-            if (window.size() > k) {
-                window.erase(nums[i - k]);
-            }
+            
+            // Add the current element to the set
+            set.insert(nums[i]);
         }
-
+        
         return false;
     }
 };
